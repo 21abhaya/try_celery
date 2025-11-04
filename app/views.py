d7c87dbe24c853d67f97e79142a0f1fc7d7175e7
abhaya <gautamabhaya98@gmail.com>
2025-11-05 01:12:43 +0545

    tinkering with celery using post request
    need to test out other applications of celry and then move on implenting a real use case

--- app/views.py
+++ app/views.py
@@ -7,10 +7,17 @@ def sum_without_celery(a, b):
 
 def test_view(request):
     
+    value = request.POST.get('value', "")
+    
     result_add = add.delay(4000000, 6000000)
     result_mul = mul.delay(4, 6)
     result_xsum = xsum.delay([1, 2, 3, 4, 5])
     result_without_celery = sum_without_celery(4, 6)
+    
+    context = {
+        'without_celery_result': result_without_celery,
+        'value': value,
+    }
     
     
     try:
@@ -18,13 +25,12 @@ def test_view(request):
         result_mul = result_mul.get(timeout=5)
         result_xsum = result_xsum.get(timeout=5)
 
-        context = {
+        context.update({
             'add_result': result_add,
             'mul_result': result_mul,
             'xsum_result': result_xsum,
-            'without_celery_result': result_without_celery,
-        }
-        
+        })
+
     except Exception as e:
         print(f"Error retrieving task results: {e}")
         context = {}
@@ -33,6 +39,7 @@ def test_view(request):
             'mul_result': 'Could not retrieve result in time',
             'xsum_result': 'Could not retrieve result in time',
             'without_celery_result': result_without_celery,
+            'value': value,
         })
 
     return render(request, 'test_view.html', context)
